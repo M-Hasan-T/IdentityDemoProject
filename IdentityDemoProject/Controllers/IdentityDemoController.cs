7ef185d6b2ac293fa028b662b58d7137148487e4
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-11 00:55:37 +0100

    Added some limitations based on Roles

---
+++ IdentityDemoProject/Controllers/IdentityDemoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDemoProject.Controllers
+{
+    [Authorize]
+    public class IdentityDemoController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin, User")]
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
